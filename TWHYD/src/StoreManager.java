dd9caceaae28cc26703d8a39afc024746eebedce
soumyadeeplogin <soumyadeeplogin@gmail.com>
2017-03-27 17:15:47 +0530

    Patch 1

--- TWHYD/src/StoreManager.java
+++ TWHYD/src/StoreManager.java
@@ -16,24 +16,24 @@ public class StoreManager {
 		finalPrice[ptr] = new DiscountCalculator().getDiscountedPrice(brandName, wearType, cost);
 	}
 	
-	private void printIt(String saleStream){
+	private String printIt(String saleStream){
 		int total = 0;
 		StringTokenizer st = new StringTokenizer(saleStream, ",");
 		while (st.hasMoreTokens()) {  
 	           total+=finalPrice[Integer.parseInt(st.nextToken())-1];
 	     }
-		System.out.println(total);
+		return String.valueOf(total);
 	}
 	
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Inv :: ");
+		//System.out.print("Inv :: ");
 		int invSize = sc.nextInt();
 		StoreManager sm = new StoreManager(invSize);
 		sc.nextLine();
 		for(int i=0; i<invSize; i++){
-			System.out.print("Line " + (i+1) + " :: ");
+			//System.out.print("Line " + (i+1) + " :: ");
 			String inp =  sc.nextLine();
 			String[] str = inp.split(",");
 			sm.priceIt(i, str[1],str[2],Double.parseDouble(str[3]));
@@ -41,12 +41,12 @@ public class StoreManager {
 		
 		int custChoices = sc.nextInt();
 		sc.nextLine();
+		String result = "";
 		for(int i=0; i<custChoices; i++){
 			String inp = sc.nextLine();
-			sm.printIt(inp);
+			result += sm.printIt(inp)+"\n";
 		}
+		System.out.println(result);
 		sc.close();
-	}
-	
-	
+	}	
 }
