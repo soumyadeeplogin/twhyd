a41696adf5b8edfc3d5409b8edb54faeca05d920
soumyadeeplogin <soumyadeeplogin@gmail.com>
2017-03-28 10:30:46 +0530

    Printer Implemented

--- PS2/TWHYDPST/src/Driver.java
+++ PS2/TWHYDPST/src/Driver.java
@@ -3,7 +3,7 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new Printer().printTicker("Kukatpally", "Charminar", 18, 46.50);
 	}
 
 }
--- PS2/TWHYDPST/src/Printer.java
+++ PS2/TWHYDPST/src/Printer.java
@@ -1,4 +1,11 @@
 
 public class Printer {
-
+	public void printTicker(String source, String destination, int distance, double cost)
+	{
+		System.out.println("*************************************************************");
+		System.out.println("			Hyderabad Metro Rail");
+		System.out.println("Source:		"+source+"		Distance ( stations): "+distance);
+		System.out.println("Destination:	"+destination+"		Cost:	\u0930 "+cost+"/-");
+		System.out.println("*************************************************************");
+	}
 }
