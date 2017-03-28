5aefe2e40d6d5132ab3da600fe3d3ca9f7d59a9d
soumyadeeplogin <soumyadeeplogin@gmail.com>
2017-03-28 11:03:11 +0530

    Added Properties

--- PS2/TWHYDPST/src/Driver.java
+++ PS2/TWHYDPST/src/Driver.java
@@ -3,7 +3,8 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new Printer().printTicker("Kukatpally", "Charminar", 18, 46.50);
+		//new Printer().printTicker("Kukatpally", "Charminar", 18, 46.50);
+		//new InputHandler().getStations();
 	}
 
 }
--- PS2/TWHYDPST/src/InputHandler.java
+++ PS2/TWHYDPST/src/InputHandler.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
 
 public class InputHandler {
-
+	public String[] getStations(){
+		
+		String[] srcdsc = new String[2];
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Source Station Code:");
+		srcdsc[0] = sc.nextLine();
+		System.out.println("Enter Destination Station Code:");
+		srcdsc[1] = sc.nextLine();
+		return srcdsc;
+		
+	}
 }
